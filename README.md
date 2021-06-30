9634bfd5a7f33610001f1f46ea61d6d517e7f0f8
Samirah.Alhusayni <Samirah.alhusayni@tuwaiq.edu.sa>
2021-06-30 18:38:50 +0300

    Strategy Pattern

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# Strategy
-this repo contains strategy design pattern implementation using C#
+
+
+![Tuwaiq](https://i.ibb.co/SV2BSn5/tuwaiq.png)
+----
+
+<br/>
+
+# Strategy Pattern
+Strategy is a behavioral design pattern that turns a set of behaviors into objects and makes them interchangeable inside original context object.
+
+<br>
+
+## Example
+
+
+<br/>
+
+## UML 
+<img src="StrategyUML.png" />
+
+## Output 
+
+<img src="Output.png" />
+
+<br/>
+
+
+## By:
+
+
+- Samirah Alhusayni
+
+
+
+
+
+
+
+
---
+++ StrategyPattern/Program.cs
@@ -0,0 +1,68 @@
+﻿
+using System;
+ 
+
+namespace StrategyPattern
+{
+    class Program
+    {
+
+            public static void Main(string[] args)
+            {
+            TransmittedWay transmittedWay;
+            Console.WriteLine("---Serial Transmission---");
+            transmittedWay = new TransmittedWay(new Serial());
+            transmittedWay.TransmittedInterface();
+            Console.WriteLine("---Parallel Transmission---");
+
+            transmittedWay = new TransmittedWay(new Parallel());
+            transmittedWay.TransmittedInterface();
+
+            }
+        }
+
+
+        public abstract class TransmissionStrategy
+        {
+            public abstract void AlgorithmInterface();
+        }
+
+
+        public class Serial : TransmissionStrategy
+    {
+            public override void AlgorithmInterface()
+            {
+                Console.WriteLine(
+                    "Data bits are organized in a specific order, " +
+                    "since they can only be sent one after another. ");
+            }
+        }
+
+
+        public class Parallel : TransmissionStrategy
+    {
+            public override void AlgorithmInterface()
+            {
+                Console.WriteLine(
+                    "Multiple data bits are transmitted" +
+                    " over multiple channels at the same time. ");
+            }
+        }
+
+
+        public class TransmittedWay
+        {
+        TransmissionStrategy strategy;
+
+            public TransmittedWay(TransmissionStrategy strategy)
+            {
+                this.strategy = strategy;
+            }
+
+            public void TransmittedInterface()
+            {
+                strategy.AlgorithmInterface();
+            }
+        }
+    }
+
